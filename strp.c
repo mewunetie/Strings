54ca3b62e459d98dafab13ec2f7727aab11d8c3f
mewunetie <90476983+mewunetie@users.noreply.github.com>
2021-10-05 15:49:59 -0400

    update

--- strp.c
+++ strp.c
@@ -80,7 +80,6 @@ size_t Str_compare(const char *s1, const char *s2) {
    return returnval;
 }
 
-/* char Str_search(const char *s1, const char *s2) {
+char *Str_search(const char *s1, const char *s2) {
 
 }
-*/
