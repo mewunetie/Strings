96e508019d0cf0e3f6e417ddac997f148e9692b7
mewunetie <90476983+mewunetie@users.noreply.github.com>
2021-10-04 22:40:12 -0400

    Update stra.c

--- stra.c
+++ stra.c
@@ -99,6 +99,7 @@ char Str_search (const char haystack[], const char needle[]) {
 
 for (i = 0; haystack[i] != '\0'; i++) {
    save = i;
+   if (needle[j] != '\0') return stringstart;
    for (j = 0; needle[j] != '\0'; j++) {
       if (haystack[i] == needle[j]) {
          stringstart = i;
@@ -111,7 +112,7 @@ for (i = 0; haystack[i] != '\0'; i++) {
          stringstart = '\0';
          i = save;
    }
-   return stringstart;
+   
 }
 
 
