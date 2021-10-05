dc71fcd89099a76d7893c89c8fa67113bd7bbf86
mewunetie <90476983+mewunetie@users.noreply.github.com>
2021-10-05 15:34:27 -0400

    trial file

--- stra.c
+++ stra.c
@@ -87,82 +87,31 @@ size_t Str_compare (const char s1[], const char s2[]){
 }
 
 /* go over this, how to ensure that the next char after the first matching is the same?
-consider doing nested loop w if == continue going and else break? */
-char *Str_search (const char haystack[], const char needle[]) {
+consider doing nested loop w if == continue going and else break? */char *Str_search (const char haystack[], const char needle[]) {
    size_t stringstart = 0;
    size_t i = 0;
    size_t j = 0;
-   size_t lengthh;
-   size_t lengthn;
+   size_t h;
+   size_t n;
 
    assert(haystack != NULL);
    assert(needle != NULL);
 
-   lengthh = Str_getLength(haystack);
-   lengthn = Str_getLength(needle);
+  h = Str_getLength(haystack);
+  n = Str_getLength(needle);
+ 
 
-
-  /* for (i = 0; i < lengthn; i++) { */
-       for (j = 0; j < lengthh; j++) {
-      if (haystack[j] == needle[i]) {
-         stringstart = j;
-          i++;
-         j++;
-         while (needle[i] == haystack[j]) {
-            if (needle[i] == '/0') {
-            return (char*) &haystack[stringstart];
-         }
-            if (haystack[j] == '/0') {
-               stringstart = 0;
-               return (char*) &haystack[stringstart];
-            }
-            i++;
-            j++;
-         }
-      
-        
-         
-    } 
-
-else if (needle[i] != haystack[j]) {
-   if (haystack[j] == '/0') {
-               stringstart = 0;
-               return (char*) &haystack[stringstart];
-            }
-   i = 0;
-   j = stringstart;
-   stringstart = 0;
-}
-
+for (int i = 0; i < h; i++) {
+    j = 1;
+    if (haystack[i] == needle[0]) {
+        stringstart = i;
+        j++;
+    }
+    if (haystack[i] == needle[j]) {
+        j++;
+    }
+    if (haystack[i] != needle[j]) {
+        j = stringstart + 1;
+    }
    }
-/* } */
-return (char*) &haystack[stringstart];
 }
-
-
-
-
-/*
- if (needle[i] == haystack[j]) {
-         stringstart = j;
-         i++;
-         j++;
-         while (needle[i] == haystack[j] && needle[i] != '\0') {
-            if (haystack[j] == '\0') {
-               stringstart = 0;
-               return (char*) &haystack[stringstart];
-            }
-            i++;
-            j++;
-         }
-      }
-
-else if (needle[i] != haystack[j] && haystack[j] != '\0' && needle[i] != '\0') {
-   j++;
-   i = 0;
-   stringstart = 0;
-}
-
-return (char*) &haystack[stringstart];
-}
-*/
