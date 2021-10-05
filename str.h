4e353c44fbfa7a9bce8c918384ae8812e6add33e
mewunetie <90476983+mewunetie@users.noreply.github.com>
2021-10-04 23:34:37 -0400

    Updated

--- str.h
+++ str.h
@@ -22,7 +22,7 @@ size_t Str_getLength(const char pcSrc[]);
 char *Str_copy(char pcDest[], const char pcSrc[]);
 char *Str_concat(char pcSrc1[], const char pcSrc2[]);
 size_t Str_compare (const char s1[], const char s2[]);
-char *Str_search (const char haystack[], const char needle[]);
+char Str_search (const char haystack[], const char needle[]);
 
 
 
